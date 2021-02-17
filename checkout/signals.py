b91a239d6ca158a9ff52bfdd7b86f9a155e1168a
Andrew Burns <burnsad@tcd.ie>
2021-02-17 09:05:27 +0000

    checkout models and signals refined

---
+++ checkout/signals.py
@@ -0,0 +1,21 @@
+import django.db.models.signals import post_save, post delete
+import django.dispatch import receiver
+
+from .models import OrderLineItem
+
+@receiver(post_save, sender=OrderLineItem)
+def update_on_save(sender, instance, created, **kwargs):
+    """
+    update order total on lineitem update/create
+    """
+
+    instance.order.update_total()
+
+
+@receiver(post_delete, sender=OrderLineItem)
+def update_on_save(sender, instance, **kwargs):
+    """
+    update order total on lineitem delete
+    """
+
+    instance.order.update_total()
