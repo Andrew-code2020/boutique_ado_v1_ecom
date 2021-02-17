64e72b73f27e58e1fdedde32d692e7ec538deb3e
Andrew Burns <burnsad@tcd.ie>
2021-02-17 17:39:18 +0000

    Views and templates part 1

--- boutique_ado_v1_ecom/settings.py
+++ boutique_ado_v1_ecom/settings.py
@@ -46,6 +46,9 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+    
+    #Other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -59,6 +62,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'boutique_ado_v1_ecom.urls'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -76,6 +80,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field'
+            ]
         },
     },
 ]
