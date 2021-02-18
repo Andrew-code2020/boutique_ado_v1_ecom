6e59087c8cd2ef260992cbc146278f288989f4b9
Andrew Burns <burnsad@tcd.ie>
2021-02-18 08:58:32 +0000

    stripe part 2 card box not rendering yet

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -5,5 +5,22 @@ var stripe_public_key = $('#id_stripe_public_key').text.slice(1,-1);
 var client_secret = $('#id_client_secret').text.slice(1,-1);
 var stripe = Stripe(stripe_public_key);
 var element = stripe.element();
-var card = element.create('card');
+var style = {
+  base: {
+    color: '#000',
+    fontSize: '16px',
+    fontFamily: '"Open Sans", sans-serif',
+    fontSmoothing: 'antialiased',
+    '::placeholder': {
+      color: '#CFD7DF',
+    },
+  },
+  invalid: {
+    color: '#dc3545',
+    ':focus': {
+      color: '#dc3545',
+    },
+  },
+};
+var card = element.create('card', {style:style});
 card.mount('#card-element')
