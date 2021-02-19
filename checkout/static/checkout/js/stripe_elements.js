2fc722fb5d30bd89c730d74f4eb99a6af9ad5712
Andrew Burns <burnsad@tcd.ie>
2021-02-19 10:26:42 +0000

    stripe keys added, exported and settings adjusted

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,27 +1,41 @@
-
-
-
-var stripe_public_key = $('#id_stripe_public_key').text().slice(1,-1);
-var client_secret = $('#id_client_secret').text().slice(1,-1);
+var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
+var client_secret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripe_public_key);
-var element = stripe.element();
-
+var elements = stripe.elements();
 var style = {
-  base: {
-    color: '#000',
-    fontSize: '16px',
-    fontFamily: '"Open Sans", sans-serif',
-    fontSmoothing: 'antialiased',
-    '::placeholder': {
-      color: '#CFD7DF',
+    base: {
+        color: '#000',
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+            color: '#aab7c4'
+        }
     },
-  },
-  invalid: {
-    color: '#dc3545',
-    ':focus': {
-      color: '#dc3545',
-    },
-  },
+    invalid: {
+        color: '#dc3545',
+        iconColor: '#dc3545'
+    }
 };
-var card = element.create('card', {style:style});
+var card = elements.create('card', { style: style });
 card.mount('#card-element');
+
+// Handle realtime validation errors
+
+card.addEventListener('change', function (event) {
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        var html = `
+        <span class="icon" role="alert">
+            <i class="fas fa-times"></i>
+        </span>
+        <span>${event.error.message}</span>
+        `;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
+
+
+
