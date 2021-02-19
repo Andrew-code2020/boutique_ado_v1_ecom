80386ab090bac5d041389809aa4c087da8622862
Andrew Burns <burnsad@tcd.ie>
2021-02-19 17:07:36 +0000

    checkout_success.html documented

--- checkout/signals.py
+++ checkout/signals.py
@@ -1,5 +1,5 @@
-import django.db.models.signals import post_save, post delete
-import django.dispatch import receiver
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
 
 from .models import OrderLineItem
 
@@ -13,9 +13,9 @@ def update_on_save(sender, instance, created, **kwargs):
 
 
 @receiver(post_delete, sender=OrderLineItem)
-def update_on_save(sender, instance, **kwargs):
+def update_on_delete(sender, instance, **kwargs):
     """
     update order total on lineitem delete
     """
-
+    print('delete signal received')
     instance.order.update_total()
--- checkout/views.py
+++ checkout/views.py
@@ -5,8 +5,6 @@ from django.conf import settings
 from .forms import OrderForm
 from .models import Order, OrderLineItem
 from products.models import Product
-
-
 from bag.contexts import bag_contents
 
 import stripe
@@ -17,7 +15,7 @@ def checkout(request):
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
     if request.method == 'POST':
-        bag = request.session.get('bag' {})
+        bag = request.session.get('bag', {})
 
         form_data = {
             'full_name': request.POST['full_name'],
@@ -31,10 +29,9 @@ def checkout(request):
             'county': request.POST['county'],
         }
         order_form = OrderForm(form_data)
-
         if order_form.is_valid():
-            order_form.save()
-            for item_id, item_data in bag items():
+            order = order_form.save()
+            for item_id, item_data in bag.items():
                 try:
                     product = Product.objects.get(id=item_id)
                     if isinstance(item_data, int):
@@ -60,12 +57,12 @@ def checkout(request):
                     )
                     order.delete()
                     return redirect(reverse('view_bag'))
+
             request.session['save_info'] = 'save-info' in request.POST
             return redirect(reverse('checkout_success', args=[order.order_number]))
-
         else:
             messages.error(request, 'There was an error with your form. \
-                Please double check your information')    
+                Please double check your information.')
     else:
         bag = request.session.get('bag', {})
         if not bag:
@@ -83,34 +80,36 @@ def checkout(request):
 
         order_form = OrderForm()
 
-        if not stripe_public_key:
-            messages.warning(request, 'Stripe public key is missing.\
-                Did you forget to set in it in your environment')
-        template = 'checkout/checkout.html'
-        context = {
-            'order_form': order_form,
-            'stripe_public_key': stripe_public_key,
-            'client_secret_key': intent.client_secret,
-        }
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
+
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
+    }
 
     return render(request, template, context)
 
 
-
 def checkout_success(request, order_number):
     """
-    Handle Successful checkouts
+    Handle successful checkouts
     """
-    save.info = request.session.get('save_info')
-    order = get.object_or_404(Order, order_number=order_number)
-    messages.success(request, f'Order sucessfully processed !!! \
-        Your order number is { order_number}. A confirmation\
-        email will be sent to {order.email}')
+    save_info = request.session.get('save_info')
+    order = get_object_or_404(Order, order_number=order_number)
+    messages.success(request, f'Order successfully processed! \
+        Your order number is {order_number}. A confirmation \
+        email will be sent to {order.email}.')
 
     if 'bag' in request.session:
         del request.session['bag']
 
     template = 'checkout/checkout_success.html'
     context = {
-        'order':order,
-    } 
+        'order': order,
+    }
+
+    return render(request, template, context)
