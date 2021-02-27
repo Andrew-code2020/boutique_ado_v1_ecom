30ee5c48a13c372a98b5df2ba51b51e4114b1953
Andrew Burns <burnsad@tcd.ie>
2021-02-27 17:00:35 +0000

    deployemnt commands set

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn boutique_ado_v1_ecom.wsgi:application
--- boutique_ado_v1_ecom/settings.py
+++ boutique_ado_v1_ecom/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'j53bgzuaz353ex_w)sg&9s2p#rie*yt_8)7zs2s)4q#=iw)zj='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['boutiqueadoandrew.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -122,12 +122,17 @@ WSGI_APPLICATION = 'boutique_ado_v1_ecom.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 
