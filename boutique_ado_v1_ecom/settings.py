2131c4a02046a8640fbc4efc7a2604b0da96c0b7
Andrew Burns <burnsad@tcd.ie>
2021-02-27 17:12:03 +0000

    deployment part 3

--- boutique_ado_v1_ecom/settings.py
+++ boutique_ado_v1_ecom/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j53bgzuaz353ex_w)sg&9s2p#rie*yt_8)7zs2s)4q#=iw)zj='
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['boutiqueadoandrew.herokuapp.com', 'localhost']
 
