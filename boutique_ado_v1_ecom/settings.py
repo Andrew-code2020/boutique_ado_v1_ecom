f0aaa52da97be99baedb20a07c13575547c4fe40
Andrew Burns <burnsad@tcd.ie>
2021-02-28 12:08:09 +0000

    deployment part 9

--- boutique_ado_v1_ecom/settings.py
+++ boutique_ado_v1_ecom/settings.py
@@ -202,9 +202,7 @@ STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 STATICFILES_LOCATION = 'static'
 DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 MEDIAFILES_LOCATION = 'media'
-
-#overide static  and media URLs in production
-
+# Override static and media URLs in production
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
